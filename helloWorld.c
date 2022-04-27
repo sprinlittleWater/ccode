12a99d7dd9d07cca9b74b1dfe5090beecbffd7ef
sprinlittleWater <104479313+sprinlittleWater@users.noreply.github.com>
2022-04-27 15:39:30 +0800

    Add files via upload

---
+++ helloWorld.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("Hello, World!\n");
+    system("pause");
+    printf("Modify source code the first time.\n");
+    system("pause");
+    
+    return 0;
+}
---
+++ helloworld.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    cout<<"Hello,VScode!"<<endl;
+    system("pause");
+    return 0;
+    
+}
